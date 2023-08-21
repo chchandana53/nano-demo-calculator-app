3f8349a4a44e02d294751fa051b4adeb8643d355
CHANDANA <chchandana53@gmail.com>
2023-08-21 15:30:55 +0530

    asdfghjk

--- server.py
+++ server.py
@@ -1,19 +1,39 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
-app = Flask(__name__)
+app = Flask(_name_)
 
+class Numbers:
+    def _init_(self, first, second):
+        self.first = first
+        self.second = second
+
+class Result:
+    def _init_(self, value):
+        self.result = value
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Hello world!"
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    data = request.get_json()
+    if data and 'first' in data and 'second' in data:
+        numbers = Numbers(data['first'], data['second'])
+        result = Result(numbers.first + numbers.second)
+        return jsonify(result._dict_)
+    else:
+        return jsonify(error="Invalid input data"), 400
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    data = request.get_json()
+    if data and 'first' in data and 'second' in data:
+        numbers = Numbers(data['first'], data['second'])
+        result = Result(numbers.first - numbers.second)
+        return jsonify(result._dict_)
+    else:
+        return jsonify(error="Invalid input data"), 400
 
-if __name__ == '__main__':
-    app.run(port=8080,host='0.0.0.0')
+if _name_ == '_main_':
+    app.run(port=8080, host='0.0.0.0')
